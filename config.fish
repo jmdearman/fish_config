bc2de99f5e3511cf8d93967d0e9aa5b7f518e4b9
James Dearman <jmdearman@gmail.com>
2017-03-07 22:09:59 -0600

    Update EDITOR to sublime.

--- config.fish
+++ config.fish
@@ -1,5 +1,6 @@
 set -g -x PATH ~/bin /usr/local/bin $PATH
-set -g -x EDITOR vi
+set -g -x EDITOR subl -w
 set -g -x PIP_REQUIRE_VIRTUALENV true
+set -g -x DEFAULT_BACKUP_DIRECTORY ~/Dropbox/Backup
 
 eval (python3 -m virtualfish)
