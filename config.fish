92cf893fb1cb6c2c9d43507cd2e58df0c5c62130
James Dearman <jmdearman@gmail.com>
2017-09-23 21:35:10 -0500

    python tweaks

--- config.fish
+++ config.fish
@@ -1,6 +1,8 @@
 set -g -x PATH ~/bin /usr/local/bin $PATH
-set -g -x EDITOR subl -w
-set -g -x PIP_REQUIRE_VIRTUALENV true
+set -g -x EDITOR "subl -w"
+set -g -x PIP_FORMAT columns
+set -g -x VIRTUAL_ENV_DISABLE_PROMPT true
 set -g -x DEFAULT_BACKUP_DIRECTORY ~/Dropbox/Backup
 
-eval (python3 -m virtualfish)
+alias python=python3
+alias pip=pip3
